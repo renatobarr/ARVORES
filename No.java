3c86a4a3cf55f6cc4e86ee57c3dc5c80fa1140c3
renatobarr <168466657+renatobarr@users.noreply.github.com>
2025-05-29 20:16:23 -0300

    balanceamento da arvore

--- No.java
+++ No.java
@@ -8,3 +8,16 @@ public class No {
         esquerda = direita = null;
     }
 }
+
+public class No {
+    String conteudo;
+    No filhoEsquerdo, filhoDireito;
+    int balanceamento;
+
+    public No(String conteudo) {
+        this.conteudo = conteudo;
+        this.filhoEsquerdo = null;
+        this.filhoDireito = null;
+        this.balanceamento = 0;
+    }
+}
